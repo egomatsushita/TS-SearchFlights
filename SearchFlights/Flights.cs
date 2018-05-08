41c6c063f91c9524644e6c1b38aa5ce229f5d639
egomatsushita <eduardo.go.rj@gmail.com>
2018-05-08 16:14:15 -0400

    Add Flights and FlightsCollections classes

---
+++ SearchFlights/Flights.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchFlights
+{
+    public class Flights
+    {
+        public string Origin { get; set; }
+        public string DepartureTime { get; set; }
+        public string Destination { get; set; }
+        public string DestinationTime { get; set; }
+        public string Price { get; set; }
+    }
+}
