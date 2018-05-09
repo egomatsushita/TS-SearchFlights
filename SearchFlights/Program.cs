1c31f98bc8ed1e88b6f0ca52e2e67fc8ba6a0593
egomatsushita <eduardo.go.rj@gmail.com>
2018-05-09 08:47:02 -0400

    Create DisplayMenu method

--- SearchFlights/Program.cs
+++ SearchFlights/Program.cs
@@ -11,5 +11,17 @@ namespace SearchFlights
         static void Main(string[] args)
         {
         }
+
+        private static void DisplayMenu(ref string inputText)
+        {
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine("* To Exit: -1                                               *");
+            Console.WriteLine("* To Search: \"searchFlights -o (Origin) -d (Destination)\"   *");
+            Console.WriteLine("*************************************************************\n");
+            Console.Write("$");
+            inputText = Console.ReadLine();
+        }
+
+
     }
 }
