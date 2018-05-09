4761cf6adf40e2db22e1079ff8fe9265725280c1
egomatsushita <eduardo.go.rj@gmail.com>
2018-05-09 11:57:17 -0400

    Test use cases

--- SearchFlights/Program.cs
+++ SearchFlights/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.IO;
 
 namespace SearchFlights
 {
@@ -68,8 +67,6 @@ namespace SearchFlights
 
                 HelperMethods.DisplayMenu(ref inputText);
             }
-        }
-
-        
+        }    
     }
 }
--- UnitTestSearchFlights/SearchFlightsTests.cs
+++ UnitTestSearchFlights/SearchFlightsTests.cs
@@ -8,12 +8,58 @@ namespace UnitTestSearchFlights
     public class SearchFlightsTests
     {
         [TestMethod]
-        public void GetProviderPath_Provider_ReturnProviderPath()
+        public void GetDelimiter_Provider_ReturnVerticalBar()
         {
             string provider = "Provider3.txt";
-            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
-            string expected = path.Replace("bin\\Debug", "Providers\\Provider3.txt");
-            string actual = get
+            char expected = '|';
+            char actual = HelperMethods.GetDelimiter(provider);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDelimiter_Provider_ReturnComma()
+        {
+            string provider = "Provider2.txt";
+            char expected = ',';
+            char actual = HelperMethods.GetDelimiter(provider);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckIfExist_DepTime_DestTime_Price_FlightData_ReturnFalse()
+        {
+            string departureTime = "6/15/2014 6:45:00";
+            string destinationTime = "6/15/2014 8:54:00";
+            string price = "$578.00";
+            FlightsCollection flightsData = new FlightsCollection();
+
+            bool result = HelperMethods.CheckIfExist(departureTime, destinationTime, price, flightsData);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CheckIfExist_DepTime_DestTime_Price_FlightData_ReturnTrue()
+        {
+            Flights flight1 = new Flights
+            {
+                Origin = "YYZ",
+                DepartureTime = "6/15/2014 6:45:00",
+                Destination = "YYC",
+                DestinationTime = "6/15/2014 8:54:00",
+                Price = "$578.00"
+            };
+            FlightsCollection flightsData = new FlightsCollection();
+            flightsData.flightsCollection.Add(flight1);
+            string departureTime = "6/15/2014 6:45:00";
+            string destinationTime = "6/15/2014 8:54:00";
+            string price = "$578.00";
+           
+            bool result = HelperMethods.CheckIfExist(departureTime, destinationTime, price, flightsData);
+
+            Assert.IsTrue(result);
         }
     }
 }
